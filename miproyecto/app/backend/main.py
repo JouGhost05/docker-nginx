069a0b98e79f27ae9d13abfaa015fd8b71ed017e
JouGhost05 <joutoscano@gmail.com>
2024-05-18 04:56:26 +0200

    He hecho cambios, ahora toca scammear

--- miproyecto/app/backend/main.py
+++ miproyecto/app/backend/main.py
@@ -1,7 +1,44 @@
-from fastapi import FastAPI
+import time
+
+time.sleep(10)
+
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
 
 app = FastAPI()
 
-@app.get("/api/saludo")
-def read_root():
-    return {"mensaje": "¡Hola desde FastAPI!"}
+DATABASE_URL = f"mysql+pymysql://admin:1234@db/mibase"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+templates = Jinja2Templates(directory="/usr/share/nginx/html")
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50))
+    email = Column(String(50))
+
+Base.metadata.create_all(bind=engine)
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/submit")
+async def submit_form(name: str = Form(...), email: str = Form(...)):
+    db = SessionLocal()
+    user = User(name=name, email=email)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    db.close()
+    return RedirectResponse("/", status_code=303)
